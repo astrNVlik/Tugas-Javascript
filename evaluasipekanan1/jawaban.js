6e290d4017f182cef3e1d56486b015f5f016ea13
astrNVlik <wertybrothers9@gmail.com>
2025-09-08 08:22:15 +0700

    Newest commit

---
+++ evaluasipekanan1/jawaban.js
@@ -0,0 +1,44 @@
+
+
+let bulan = 1
+switch (bulan) {
+
+case 1: 
+console.log("Mie goreng")
+break;
+case 2:
+console.log("Seblak")
+break;
+case 3: 
+console.log("Kopling innova")
+break;
+case 4: 
+console.log("Piscok")
+break;
+case 5: 
+console.log("Rendang")
+break;
+case 6: 
+console.log("Cumi tepung")
+break;
+case 7: 
+console.log("Jus manggo")
+break;
+case 8: 
+console.log("Jus sirsak")
+break;
+case 9: 
+console.log("Es teh")
+break;
+case 10: 
+console.log("Teh anget")
+break;
+case 11: 
+console.log("Air putih")
+break;
+case 12: 
+console.log("Kopi hitam")
+break;
+default:
+    console.log("belum tersedia.")
+}
---
+++ latihansoalday5/soalteori.md
@@ -0,0 +1,19 @@
+Apa fungsi utama JavaScript dalam sebuah website?
+a. Membuat struktur halaman
+b. Menambahkan gaya/tampilan
+c. Membuat website interaktif
+d. Menghubungkan ke database
+
+jawaban : C
+
+Manakah yang termasuk framework JavaScript?
+a. React
+b. jQuery
+c. Lodash
+d. Bootstrap
+
+jawaban : A
+
+Sebutkan 2 keunggulan JavaScript.
+
+-bisa digunakan langsung diweb
